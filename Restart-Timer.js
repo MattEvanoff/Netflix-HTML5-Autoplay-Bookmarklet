11700c39da2c6ede1c5a227a7df620258edab801
Matt Evanoff <rtpmatt+github@gmail.com>
2015-03-21 03:21:32 -0400

    Adding a dialog for the restart timer

--- Restart-Timer.js
+++ Restart-Timer.js
@@ -1,4 +1,21 @@
 //Restart Timer
 (function() {
+	if(!window._ME || !window._ME.autoplayer) {
+		alert('You never started the actual autoplayer dumbass.');
+		return;
+	}
 	window._ME.autoplayer.startTime = new Date();
+
+	if (!document.getElementById('_ME-MSG')) {
+		var mydiv = document.body.appendChild(document.createElement('div'));
+		mydiv.id = '_ME-MSG';
+		mydiv.innerHTML = 'Sleep Timer Reset For '+ window._ME.autoplayer.playTime / 60 / 1000 +' Minutes';
+		mydiv.style.cssText = 'position: fixed; text-align: center; top: 100px; border: 30px; font-size: 30px; color: #FFFFFF; padding: 15px; margin: auto auto auto -150px; width: 300px; left: 50%; background-color: #000000;'
+	} 
+
+	document.getElementById('_ME-MSG').style.display = 'block';
+
+	setTimeout(function(){
+		document.getElementById('_ME-MSG').style.display = 'none';
+	}, 3000);
 })();
